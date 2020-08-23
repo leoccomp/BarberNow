51a5c64fa039fd69caeb9bb2761890ca0c3d7fdc
Leonardo <leoccomp@hotmail.com>
2020-08-23 17:46:31 -0300

    User create

--- backend/src/database/migrations/1598198051610-CreateUsers.ts
+++ backend/src/database/migrations/1598198051610-CreateUsers.ts
@@ -1,16 +1,10 @@
-import {
-    MigrationInterface,
-    QueryRunner,
-    Table,
-    DefaultNamingStrategy,
-} from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1598034889976
-    implements MigrationInterface {
+export default class CreateUsers1598198051610 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns: [
                     {
                         name: 'id',
@@ -20,13 +14,19 @@ export default class CreateAppointments1598034889976
                         default: 'uuid_generate_v4()',
                     },
                     {
-                        name: 'provider',
+                        name: 'name',
                         type: 'varchar',
                         isNullable: false,
                     },
                     {
-                        name: 'date',
-                        type: 'timestamp with time zone',
+                        name: 'email',
+                        type: 'varchar',
+                        isNullable: false,
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
                         isNullable: false,
                     },
                     {
@@ -45,6 +45,6 @@ export default class CreateAppointments1598034889976
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users');
     }
 }
