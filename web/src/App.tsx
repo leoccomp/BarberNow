08424ccfa7bb67b3c874fb9af24cc3f19a2e0b70
Leonardo <leoccomp@hotmail.com>
2020-08-24 17:45:09 -0300

    Initial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+import SignIn from './pages/signIn';
+
+const App: React.FC = () => (
+    <>
+        <SignIn />
+        <GlobalStyle />
+  </>
+);
+
+export default App;
